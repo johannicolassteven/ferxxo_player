ae04edea7292470c8776330bddaef5ceebcb94b7
johannicolassteven <jnespinosa1@misena.edu.co>
2022-12-04 22:13:25 -0500

    inicio javascript

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-// main_songs : {
+const songList = [ 
+    {
+        title: "De tanto chimbiar",
+        file: ".mp3",
+        cover: "img1"
+    },
+    {
+        title: "Quemando calorias",
+        file: ".mp3",
+        cover: "img1"
+    },
+    {
+        title: "Aguante",
+        file: ".mp3",
+        cover: "img1"
+    }
+]
 
-// song1: {
-//     name: song1,
-//     audio: archivo.mp3
-// }
-
-
-
-
-
-// }
+//
