94102cf0758ad7b0cbb6c50d11dc357159f680ee
johannicolassteven <jnespinosa1@misena.edu.co>
2022-12-06 20:00:19 -0500

    js

--- index.js
+++ index.js
@@ -16,4 +16,25 @@ const songList = [
     }
 ]
 
-//
+//Capturar elementos del DOM
+const songs = document.getElementsByClassName("songs")
+
+//cargar canciones y mostrar listado
+
+function loadSongs(){
+    songList.forEach((song, index) => {
+        console.log(index)
+        const li = document.createElement("li")
+        const link = document.createElement("a")
+        link.textContent = song.title
+        link.href = "#"
+        li.appendChild(link)
+        songs.appendChild(li)
+        
+    })
+}
+
+//go
+
+loadSongs()
+
